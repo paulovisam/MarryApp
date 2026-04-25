864bb5300ca682240f0568f0bcdcaabef50fb16e
Paulo Mendonça <paulo.mendonca@visie.com.br>
2026-04-24 23:13:18 -0300

    response webhook

--- api/webhook.js
+++ api/webhook.js
@@ -98,6 +98,7 @@ export default async function handler(req, res) {
                 }
             }
         }
+        res.status(200).json({ received: true });
     } catch (error) {
         console.error('Webhook Error:', error);
         res.status(500).json({ error: error.message });
